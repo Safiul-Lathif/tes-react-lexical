439ed828c40c74d2f1679b4fd1b4a0845cff2e22
Safiul-Lathif <safiullathif65@gmail.com>
2025-01-16 17:32:10 +0530

    added every single file

---
+++ src/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,72 @@
+import { Liveblocks } from "@liveblocks/node";
+import { NextRequest } from "next/server";
+
+/**
+ * Authenticating your Liveblocks application
+ * https://liveblocks.io/docs/authentication
+ */
+
+const liveblocks = new Liveblocks({
+  secret: "sk_dev_W15xPooPVekoJbgssi6V2A1_Yro6caodT24r0tjedweeFAuO8h2Yy16xBdXSZTTg",
+});
+
+export async function POST(request: NextRequest) {
+  // Get the current user's unique id from your database
+  const userId = Math.floor(Math.random() * 10) % USER_INFO.length;
+
+  // Create a session for the current user
+  // userInfo is made available in Liveblocks presence hooks, e.g. useOthers
+  const session = liveblocks.prepareSession(`user-${userId}`, {
+    userInfo: USER_INFO[userId],
+  });
+
+  // Use a naming pattern to allow access to rooms with a wildcard
+  session.allow(`liveblocks:examples:*`, session.FULL_ACCESS);
+
+  // Authorize the user and return the result
+  const { body, status } = await session.authorize();
+  return new Response(body, { status });
+}
+
+const USER_INFO = [
+  {
+    name: "Safiul Lathif",
+    color: "#D583F0",
+    picture: "https://liveblocks.io/avatars/avatar-1.png",
+  },
+  {
+    name: "Roja",
+    color: "#F08385",
+    picture: "https://liveblocks.io/avatars/avatar-2.png",
+  },
+  {
+    name: "Pavithra",
+    color: "#F0D885",
+    picture: "https://liveblocks.io/avatars/avatar-3.png",
+  },
+  {
+    name: "Prakash",
+    color: "#85EED6",
+    picture: "https://liveblocks.io/avatars/avatar-4.png",
+  },
+  {
+    name: "Vignesh",
+    color: "#85BBF0",
+    picture: "https://liveblocks.io/avatars/avatar-5.png",
+  },
+  {
+    name: "Ilayan Sudersan",
+    color: "#8594F0",
+    picture: "https://liveblocks.io/avatars/avatar-6.png",
+  },
+  {
+    name: "Varthan",
+    color: "#85DBF0",
+    picture: "https://liveblocks.io/avatars/avatar-7.png",
+  },
+  {
+    name: "Prakssh",
+    color: "#87EE85",
+    picture: "https://liveblocks.io/avatars/avatar-8.png",
+  },
+];
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Room } from "@/app/Room";
+import styles from "@/components/Editor.module.css";
+import CollaborativeEditor from "@/components/Editor";
+import { useRoom, useSelf } from "@liveblocks/react/suspense";
+
+// Learn how to structure your collaborative Next.js app
+// https://liveblocks.io/docs/guides/how-to-use-liveblocks-with-nextjs-app-directory
+
+export default function Page() {
+  return (
+    <main>
+      <div className={styles.container}>
+        <div className={styles.editorContainer}>
+          <Room>
+            <CollaborativeEditor />
+          </Room>
+        </div>
+      </div>
+    </main>
+  );
+}
